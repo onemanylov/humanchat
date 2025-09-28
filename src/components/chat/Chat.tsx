2f8ca6dd75ff1d1f3479b0505370d11f7d7701c9
manylovv <yellowmanylov@gmail.com>
2025-09-28 17:01:59 +0200

    ui adjustments for ios

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -29,7 +29,7 @@ export default function Chat() {
       </div>
 
       {/* Fixed floating input at bottom */}
-      <div className="fixed right-0 bottom-4 left-0 z-40">
+      <div className="fixed right-2 bottom-4 left-2 z-40">
         <ChatInput onValidationChange={setInputValidation} />
       </div>
 
