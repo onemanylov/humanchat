84f9981229257cbf496754cd338b4f8044f98da6
manylovv <yellowmanylov@gmail.com>
2025-09-28 14:27:36 +0200

    postgres instead of sqlite

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,7 +1,8 @@
-import Database from 'better-sqlite3';
-import { drizzle } from 'drizzle-orm/better-sqlite3';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
 
-const databasePath = process.env.DATABASE_URL || './.data/sqlite.db';
-const sqlite = new Database(databasePath);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
-export const db = drizzle(sqlite);
+export const db = drizzle(pool);
